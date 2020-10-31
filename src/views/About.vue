929cfc16c06a350a8b1efc88f10121da51fa4f2f
anatoliybytskiyy <anatoliybytskiyy@gmail.com>
2020-10-31 20:14:59 +0300

    add mdb

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <mdb-input type="text" label="Basic example" outline />
   </div>
 </template>
+<script>
+import { mdbInput } from 'mdbvue'
+
+export default {
+  name: 'InputsPage',
+  components: {
+    mdbInput
+  }
+}
+</script>
